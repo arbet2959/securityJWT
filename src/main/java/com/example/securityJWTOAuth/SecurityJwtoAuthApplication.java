b672c8f31540e5e8dcc637f4265cd263bc421c1a
Arbet2959 <wlsl3472@naver.com>
2024-06-25 16:57:35 +0900

    first commit - 스프링시큐리티 jwt redis oauth2 기초세팅

---
+++ src/main/java/com/example/securityJWTOAuth/SecurityJwtoAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityJWTOAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtoAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtoAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securityJWTOAuth/SecurityJwtoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityJWTOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtoAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
